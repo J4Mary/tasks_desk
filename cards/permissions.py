b3ddeb879486e896c0f562249f5ecdbebfceb228
J4Mary <masha.pichienko@gmail.com>
2020-08-14 17:27:34 +0300

    some changings in html and permissions

--- cards/permissions.py
+++ cards/permissions.py
@@ -3,7 +3,13 @@ from rest_framework import permissions
 
 class CreatedByAccessPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        ALLOWED_METHODS = ['PUT', 'PATCH']
-        if request.method in ALLOWED_METHODS and obj.created_by == request.user:
+        if request.method in ['GET', 'HEAD', 'OPTIONS'] and obj.created_by == request.user:
+            return True
+        elif request.method in ['PUT', 'PATCH'] and obj.created_by == request.user and obj.status < 4:
+            return True
+        return request.user.is_superuser
+
+    def has_permission(self, request, view):
+        if request.user.active:
             return True
         return request.user.is_superuser
