925cad8d3bce5ebcc42bb45b2e9222fce1f8104a
J4Mary <masha.pichienko@gmail.com>
2020-08-14 14:00:32 +0300

    some changings in settings

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -133,3 +133,14 @@ SESSION_TIMEOUT = 3000
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
